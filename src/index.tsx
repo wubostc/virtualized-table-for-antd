f537db668c8fbde64bae6c762702a9a21c806521
wubo <913721086@qq.com>
2019-11-22 17:22:54 +0800

    fix a bug.(#26)

--- src/index.tsx
+++ src/index.tsx
@@ -1275,6 +1275,7 @@ function init(id: number) {
       }
     };
     // set the default implementation layer.
+    inside.components = {};
     _set_components(inside, {
       table: Table,
       body: {
