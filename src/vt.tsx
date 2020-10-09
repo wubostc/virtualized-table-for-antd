d20910800be570f512d7b3733252cc68079c7710
h-hosaka <68316803+KoseiHosaka@users.noreply.github.com>
2020-10-09 14:39:10 +0900

    Update vt.tsx
    change offsetHeight to BoundingClientRect().height

--- src/vt.tsx
+++ src/vt.tsx
@@ -721,7 +721,7 @@ function VTRow<T>(props: VRowProps<T>) {
 
 
   useEffect(() => {
-    const h = inst.current.offsetHeight;
+    const h = Math.ceil(inst.current.getBoundingClientRect().height); // change from fb9f71f
     const curr_h = ctx.row_height[index];
     const last_h = ctx.row_height[last_index.current];
 
